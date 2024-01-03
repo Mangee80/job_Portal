3e48b24223904943f78d9e23980b568167d4b427
Mani Pratap Singh <sDemanipratap@gmail.com>
2024-01-03 15:52:30 +0530

    Add Job page

--- client/jpa/src/App.jsx
+++ client/jpa/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import { Register } from "./pages/Register";
+import { AddJob } from "./pages/Jobform"
 import { JobListing } from "./pages/Listings";
 import {Login} from "./pages/Login";
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/addJob" element={<AddJob/>}/>
       <Route path="/listing" element={<JobListing/>}/>
       <Route path="/" element={<JobListing/>}/>
     </Routes>
